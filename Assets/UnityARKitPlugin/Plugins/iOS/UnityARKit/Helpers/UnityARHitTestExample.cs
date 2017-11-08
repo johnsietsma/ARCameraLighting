018bfccfd05bfd881b4a0c6533053d1b42c763fe
John Sietsma <john.sietsma@gmail.com>
2017-11-08 12:45:45 +0530

    Enable face toward on ARKit and other minor fixes.

--- Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARHitTestExample.cs
+++ Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARHitTestExample.cs
@@ -17,6 +17,7 @@ namespace UnityEngine.XR.iOS
                     Debug.Log ("Got hit!");
                     m_HitTransform.position = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
                     m_HitTransform.rotation = UnityARMatrixOps.GetRotation (hitResult.worldTransform);
+					FaceToward.SetFacing(m_HitTransform, Camera.main.transform.position);
 					m_PlaneTapped.Invoke ();
                     Debug.Log (string.Format ("x:{0:0.######} y:{1:0.######} z:{2:0.######}", m_HitTransform.position.x, m_HitTransform.position.y, m_HitTransform.position.z));
                     return true;
