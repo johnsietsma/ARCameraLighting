651767f24464726133d088eb8c1b9c8cf6a2500e
John Sietsma <john.sietsma@gmail.com>
2017-12-19 10:44:25 +1100

    Put the unified AR interface in a separate folder to the AR camera lighting.

---
+++ Assets/UnifiedAR/Scripts/AREditorCamera.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+#if UNITY_EDITOR
+
+public class AREditorCamera : MonoBehaviour, IARCamera
+{
+    [Range(0, 1)]
+    public float pixelIntensity = 0.5f;
+
+    public Texture defaultTexture;
+
+    private Material blitMat;
+
+    public Camera Camera
+    {
+        get
+        {
+            Debug.Assert(Camera.main != null, "You must have a Camera tagged as MainCamera in your scene.");
+            return Camera.main;
+        }
+    }
+
+    public float LightEstimation { get { return pixelIntensity; } }
+
+    public void BlitCameraTexture(CommandBuffer commandBuffer, int destinationTextureID)
+    {
+        if (blitMat == null) LoadBlitMat();
+        commandBuffer.Blit(defaultTexture, destinationTextureID, blitMat);
+    }
+
+    private void LoadBlitMat()
+    {
+        // Load up the material that does the blit.
+        blitMat = Resources.Load<Material>("Materials/ARCoreBlit");
+        Debug.Assert(blitMat);
+    }
+}
+
+#endif
